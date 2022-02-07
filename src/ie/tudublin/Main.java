35f86bbafef60619b1e325c6acb9520cd32b6446
Luke Moss Hughes <lukejohnmosshughes@gmail.com>
2022-02-07 12:28:16 +0000

    inital commit

---
+++ src/ie/tudublin/Main.java
@@ -0,0 +1,12 @@
+package ie.tudublin;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Every little cell in my body is happy");
+    }
+
+}
